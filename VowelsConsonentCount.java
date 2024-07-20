98ff2b30e5ca808b248f8722d9bee664f92f38db
Hema7222 <hemajeya004@gmail.com>
2024-07-20 20:07:19 +0530

    Count V&C without Repetitions

---
+++ VowelsConsonentCount.java
@@ -0,0 +1,56 @@
+//package Java_Practice_codes;
+//
+//public class VowelsConsonentCount {
+//
+//	public static void main(String[] args) {
+//		String str = "Biodegradations";
+//		int vowels=0, consonent=0;
+//		str = str.toLowerCase();
+//		for(char c : str.toCharArray()) {
+//			if(c>='a' && c<='z') {
+//				if(c=='a' || c=='e' || c=='i' || c=='o' ||c=='u') {
+//					vowels++;
+//				}
+//				else {
+//					consonent++;
+//				}
+//			}
+//		}
+//		System.out.println("Vowels: "+vowels);
+//		System.out.println("Consonent: "+consonent);
+//	}
+//
+//}
+
+
+
+
+package Java_Practice_codes;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VowelsConsonentCount {
+
+	public static void main(String[] args) {
+		String str = "Biodegradations";
+		int vowels=0, consonent=0;
+		Set<Character> uniqueChars = new HashSet<>();
+		str = str.toLowerCase();
+		for(char c : str.toCharArray()) {
+			if(c>='a' && c<='z' && !uniqueChars.contains(c)) {
+				uniqueChars.add(c);
+				if(c=='a' || c=='e' || c=='i' || c=='o' ||c=='u') {
+					vowels++;
+				}
+				else {
+					consonent++;
+				}
+			}
+		}
+		System.out.println("Vowels: "+vowels);
+		System.out.println("Consonent: "+consonent);
+	}
+
+}
+
